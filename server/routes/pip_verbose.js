b2bbc1e5e473adcf70c1bc182a360c99debb7e15
Thao Nguyen <thao.nguyen@deliveree.com>
2025-09-08 14:05:58 +0700

    [feat]: fixing conflict code

--- server/routes/pip_verbose.js
+++ server/routes/pip_verbose.js
@@ -36,9 +36,8 @@ module.exports = function (req, res) {
   // rewrite response to emulate 'wof-admin-lookup' format
   let resp = {}
   rows.forEach(row => {
-    let centroid = row.centroid.split(',').map(util.floatPrecision7)
-    let name = row.name_localized || row.name || undefined
-    let admin_level = row.admin_level || undefined
+    const name = row.name_localized || row.name || undefined
+    const admin_level = row.admin_level || undefined
 
     let nameAlias = []
     if (query.aliaslimit > 0) { nameAlias = (row.names || '').split(String.fromCharCode(30)) }
